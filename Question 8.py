bd2978808576d7211cfc4b041c6bd7518979a9d0
Saga <saga.s.ibrahim@gmail.com>
2026-02-03 18:40:53 -0700

    add

--- Question 8.py
+++ Question 8.py
@@ -1,1 +1,26 @@
 #QUESTION 8
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://en.wikipedia.org/wiki/Data_science'
+headers = {'User-Agent': 'Mozilla/5.0'}
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+content_area = soup.find('div', id='mw-content-text')
+
+exclude_list = ["References", "External links", "See also", "Notes"]
+headings = []
+
+if content_area:
+    for h2 in content_area.find_all('h2'):
+        text = h2.get_text().replace('[edit]', '').strip()
+
+        if not any(exclude in text for exclude in exclude_list) and text:
+            headings.append(text)
+
+    with open('headings.txt', 'w', encoding='utf-8') as file:
+        for heading in headings:
+            file.write(heading + '\n')
+
+    print("Success! headings.txt has been created.")
